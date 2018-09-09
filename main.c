03103bcb9403c7d0e7b972e8a043aa088d3689d0
Adnan Vatric <vatric_adnan@hotmail.com>
2018-09-10 00:37:50 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,54 @@
+//
+// Created by Adnan Vatric on 09.09.2018.
+//
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "progress.h"
+
+#ifdef _WIN32
+    #include <windows.h>
+	#define wait(X) (Sleep(X))
+#else
+    #include <unistd.h>
+    #define wait(X) usleep(X*1000)
+#endif
+
+#define PROCESS_COUNT 4
+
+int main(void) {
+    unsigned int percent = 0;
+    unsigned int progress = 0;
+    unsigned int sign_nr = 0;
+
+    char signs[4] = {'-', '\\', '|', '/'};
+
+    init_io();
+
+    while(percent < 100) {
+#ifdef DEBUG
+        wait_key();
+#else
+        wait(50);
+#endif
+
+        float sum = 0;
+        progress = get_progress();
+
+        for(int i = 0; i < PROCESS_COUNT; i++)
+            sum += (progress >> (i * 8)) & 0xFF;
+
+        percent = (unsigned int) ((sum / (255 * PROCESS_COUNT)) * 100);
+
+        progressbar('=', signs[sign_nr], ' ', percent);
+
+        if(sign_nr < 3)
+            sign_nr++;
+        else
+            sign_nr = 0;
+    }
+
+    shutdown_io();
+
+    return 0;
+}
